885329a578c37b701f2b54be074cfc50b6813a7f
Brenno Dantas Viana <brennoviana019@gmail.com>
2026-01-14 21:28:43 -0300

    Add files via upload

---
+++ AutoClick/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace AutoClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.minuteInterval = new System.Windows.Forms.TextBox();
+            this.secondInterval = new System.Windows.Forms.TextBox();
+            this.msInterval = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.repeatUntil = new System.Windows.Forms.RadioButton();
+            this.repeatUntilStopped = new System.Windows.Forms.RadioButton();
+            this.repeatTimes = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.githubLink = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.startHotkey = new System.Windows.Forms.Button();
+            this.stopHotkey = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mouseButtonOption = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stop_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // minuteInterval
+            // 
+            this.minuteInterval.Location = new System.Drawing.Point(69, 60);
+            this.minuteInterval.Name = "minuteInterval";
+            this.minuteInterval.Size = new System.Drawing.Size(44, 20);
+            this.minuteInterval.TabIndex = 2;
+            this.minuteInterval.Text = "1";
+            this.minuteInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.minuteInterval.TextChanged += new System.EventHandler(this.minuteInterval_TextChanged);
+            // 
+            // secondInterval
+            // 
+            this.secondInterval.Location = new System.Drawing.Point(170, 60);
+            this.secondInterval.Name = "secondInterval";
+            this.secondInterval.Size = new System.Drawing.Size(44, 20);
+            this.secondInterval.TabIndex = 3;
+            this.secondInterval.Text = "1";
+            this.secondInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.secondInterval.TextChanged += new System.EventHandler(this.secondInterval_TextChanged_1);
+            // 
+            // msInterval
+            // 
+            this.msInterval.Location = new System.Drawing.Point(264, 60);
+            this.msInterval.Name = "msInterval";
+            this.msInterval.Size = new System.Drawing.Size(61, 20);
+            this.msInterval.TabIndex = 4;
+            this.msInterval.Text = "1";
+            this.msInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.msInterval.TextChanged += new System.EventHandler(this.msInterval_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(66, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Minutes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(170, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seconds";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(261, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Milliseconds";
+            // 
+            // repeatUntil
+            // 
+            this.repeatUntil.AutoSize = true;
+            this.repeatUntil.Location = new System.Drawing.Point(436, 40);
+            this.repeatUntil.Name = "repeatUntil";
+            this.repeatUntil.Size = new System.Drawing.Size(60, 17);
+            this.repeatUntil.TabIndex = 8;
+            this.repeatUntil.TabStop = true;
+            this.repeatUntil.Text = "Repeat";
+            this.repeatUntil.UseVisualStyleBackColor = true;
+            this.repeatUntil.CheckedChanged += new System.EventHandler(this.repeatUntil_CheckedChanged);
+            // 
+            // repeatUntilStopped
+            // 
+            this.repeatUntilStopped.AutoSize = true;
+            this.repeatUntilStopped.Location = new System.Drawing.Point(436, 77);
+            this.repeatUntilStopped.Name = "repeatUntilStopped";
+            this.repeatUntilStopped.Size = new System.Drawing.Size(123, 17);
+            this.repeatUntilStopped.TabIndex = 9;
+            this.repeatUntilStopped.TabStop = true;
+            this.repeatUntilStopped.Text = "Repeat until stopped";
+            this.repeatUntilStopped.UseVisualStyleBackColor = true;
+            this.repeatUntilStopped.CheckedChanged += new System.EventHandler(this.repeatUntilStopped_CheckedChanged);
+            // 
+            // repeatTimes
+            // 
+            this.repeatTimes.Location = new System.Drawing.Point(502, 40);
+            this.repeatTimes.Name = "repeatTimes";
+            this.repeatTimes.Size = new System.Drawing.Size(44, 20);
+            this.repeatTimes.TabIndex = 10;
+            this.repeatTimes.Text = "1";
+            this.repeatTimes.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(553, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "times";
+            // 
+            // githubLink
+            // 
+            this.githubLink.AutoSize = true;
+            this.githubLink.Location = new System.Drawing.Point(48, 297);
+            this.githubLink.Name = "githubLink";
+            this.githubLink.Size = new System.Drawing.Size(87, 13);
+            this.githubLink.TabIndex = 13;
+            this.githubLink.TabStop = true;
+            this.githubLink.Text = "Github repositore";
+            this.githubLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.githubLink_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AutoClick.Properties.Resources.GitHub_Symbol;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 291);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(28, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Interval per click";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.startHotkey);
+            this.groupBox2.Controls.Add(this.stopHotkey);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.mouseButtonOption);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox2.Location = new System.Drawing.Point(28, 129);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(328, 136);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Click options";
+            // 
+            // startHotkey
+            // 
+            this.startHotkey.Location = new System.Drawing.Point(111, 53);
+            this.startHotkey.Name = "startHotkey";
+            this.startHotkey.Size = new System.Drawing.Size(75, 23);
+            this.startHotkey.TabIndex = 23;
+            this.startHotkey.Text = "button3";
+            this.startHotkey.UseVisualStyleBackColor = true;
+            this.startHotkey.Click += new System.EventHandler(this.startHotkey_Click);
+            // 
+            // stopHotkey
+            // 
+            this.stopHotkey.Location = new System.Drawing.Point(111, 87);
+            this.stopHotkey.Name = "stopHotkey";
+            this.stopHotkey.Size = new System.Drawing.Size(75, 23);
+            this.stopHotkey.TabIndex = 22;
+            this.stopHotkey.Text = "button1";
+            this.stopHotkey.UseVisualStyleBackColor = true;
+            this.stopHotkey.Click += new System.EventHandler(this.stopHotkey_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(20, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Stop hotkey";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(20, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Start hotkey";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(20, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Mouse button";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // mouseButtonOption
+            // 
+            this.mouseButtonOption.FormattingEnabled = true;
+            this.mouseButtonOption.Items.AddRange(new object[] {
+            "Left",
+            "Right"});
+            this.mouseButtonOption.Location = new System.Drawing.Point(115, 19);
+            this.mouseButtonOption.Name = "mouseButtonOption";
+            this.mouseButtonOption.Size = new System.Drawing.Size(121, 21);
+            this.mouseButtonOption.TabIndex = 19;
+            this.mouseButtonOption.SelectedIndexChanged += new System.EventHandler(this.mouseButtonOption_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(409, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(235, 100);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Repeat options";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(409, 134);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(235, 59);
+            this.startBtn.TabIndex = 19;
+            this.startBtn.Text = "Start click";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stop_btn
+            // 
+            this.stop_btn.Location = new System.Drawing.Point(409, 221);
+            this.stop_btn.Name = "stop_btn";
+            this.stop_btn.Size = new System.Drawing.Size(235, 59);
+            this.stop_btn.TabIndex = 20;
+            this.stop_btn.Text = "Stop click";
+            this.stop_btn.UseVisualStyleBackColor = true;
+            this.stop_btn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(725, 333);
+            this.Controls.Add(this.stop_btn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.githubLink);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.repeatTimes);
+            this.Controls.Add(this.repeatUntilStopped);
+            this.Controls.Add(this.repeatUntil);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.msInterval);
+            this.Controls.Add(this.secondInterval);
+            this.Controls.Add(this.minuteInterval);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox minuteInterval;
+        private System.Windows.Forms.TextBox secondInterval;
+        private System.Windows.Forms.TextBox msInterval;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton repeatUntil;
+        private System.Windows.Forms.RadioButton repeatUntilStopped;
+        private System.Windows.Forms.TextBox repeatTimes;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel githubLink;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox mouseButtonOption;
+        private System.Windows.Forms.Button startHotkey;
+        private System.Windows.Forms.Button stopHotkey;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stop_btn;
+    }
+}
+
---
+++ AutoClick/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace AutoClick
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const uint MOUSEEVENTF_LEFTUP = 0x0004;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+            
+
+
+        private void githubLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            githubLink.LinkArea = new LinkArea(0, githubLink.Text.Length);
+            System.Diagnostics.Process.Start("https://github.com/brenno-d");
+        }
+
+        private void msInterval_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minuteInterval_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secondInterval_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void repeatUntil_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void repeatUntilStopped_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startHotkey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopHotkey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mouseButtonOption_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
